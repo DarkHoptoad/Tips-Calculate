b9a70639505bba48099ec156ddfa851e28c3e906
DarkFrog <demennikov.ivan@gmail.com>
2022-10-09 16:08:19 +0000

    first commit

---
+++ src/components/Wrapper/Wrapper.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Main } from "../Main/Main";
+
+import "./Wrapper.css";
+
+const Wrapper = () => {
+  return (
+    <div className="wrapper">
+      <span>Spli</span> <span className="span">Terr</span>
+      <Main />
+    </div>
+  );
+};
+
+export { Wrapper };
